01458b5cd72b26f340d952034d64d09b80a9a112
TanguyLichau <tanguylichau@gmail.com>
2023-01-14 12:43:51 +0100

    Now can create an empty commit

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
+import sys
+import subprocess
 
 username = "YourGitHubUsername"
 password = "YourGitHubPassword"
 
-repo_name = os.argv[1]
+repo_name = sys.argv[1]
 repo_description = "This is my new repository"
 
 if not os.path.exists(repo_name):
     os.makedirs(repo_name)
 
 os.chdir(repo_name)
+
+
+subprocess.run(["git", "init"])
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "--allow-empty",  "-m", "First Commit"])
